99c89c222559982c4cb12fd9c9d88499bfef6040
Vik Ramanujam <vik@ramanuj.am>
2018-09-20 15:13:46 +0800

    Init conf-env

---
+++ .configrc.js
@@ -0,0 +1,21 @@
+export default {
+    port: 8081,
+    serverUrl: 'http://10.214.129.20:8080',
+    serverPath: '/',
+    useProxy: true,
+    proxyIP: '10.20.15.213',
+    proxyPort: 3128,
+    POCServer: 'localhost',
+    nuanceServer: 'http://10.214.129.20:8091',
+    nuanceLicensePort: 8090,
+    xstreamServer: 'https://10.214.98.50',
+    xstreamServer: 'https://10.214.98.50',
+    xstreamServer: 'https://10.214.98.50',
+    operatingMode: 'fast',
+    time: async () => (new Date()).now,
+    auth: { 
+        username: 'user1', 
+        password: 'Passw0rd01'
+    },
+};
+
